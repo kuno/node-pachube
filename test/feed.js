9ac418ab724fee535042eb402b0482cc1bf31633
kuno <neokuno@gmail.com>
2011-04-01 08:38:51 +0800

    added ncdoc generated documents

--- test/feed.js
+++ test/feed.js
@@ -1,7 +1,12 @@
+var MASTERAPIKEY = 'WANKT8pJLP-2xFrw5EqvQZBJg_Gi24hTyim9uY0GRUk';
 var Feed = require('../lib/feed.js'),
-    api_key = 'WANKT8pJLP-2xFrw5EqvQZBJg_Gi24hTyim9uY0GRUk';
-var feed = new Feed(api_key);
+    feed = new Feed(MASTERAPIKEY);
+
 var parameters = [{key:"lat",value:51.52},{key:"lon",value:0.13},{key:"distance",value:100.0},{key:"q",value:'energy'}];
+
 feed.list(parameters, function(data) {
-    console.log(data);
+    var result = JSON.parse(data);
+    while (result[k]) { 
+      console.log(k);
+    }
 });
