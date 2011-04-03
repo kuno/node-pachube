70adf2161a1e7b958ee8db73546a6c3aa0eca6c4
kuno <neokuno@gmail.com>
2011-04-03 12:15:34 +0800

    make http request accept file path, string or object as data source.

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-var fs          = require('fs'),
-    path        = require('path');
+var fs             = require('fs'),
+    path           = require('path');
 
-var User        = require('./lib/user.js'),
-    Feed        = require('./lib/feed.js'),
-    Point       = require('./lib/point.js'),
-    Stream      = require('./lib/stream.js'),
-    Trigger     = require('./lib/trigger.js');
+var User           = require('./lib/user.js'),
+    Feed           = require('./lib/feed.js'),
+    Point          = require('./lib/point.js'),
+    Stream         = require('./lib/stream.js'),
+    Trigger        = require('./lib/trigger.js');
 
-exports.User    = User;
-exports.Feed    = Feed;
-exports.Point   = Point;
-exports.Stream  = Stream;
-exports.Trigger = Trigger;
+exports.User       = User;
+exports.Feed       = Feed;
+exports.Point      = Point;
+exports.Stream     = Stream;
+exports.Trigger    = Trigger;
 
-exports.version = JSON.parse(fs.readFileSync(path.join(__dirname, './package.json'))).version;
+exports.version    = JSON.parse(fs.readFileSync(path.join(__dirname, './package.json'))).version;
+exports.apiVersion = require('./include/meta.js').apiVersion;
